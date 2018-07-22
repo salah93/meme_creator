b22ea4f37734a4c324bdef225a80c85977bb7efd
salah ahmed <salah.ahmed.hslc@gmail.com>
2018-07-22 13:17:36 -0400

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
+const browserSync = require('browser-sync').create();
+const composer = require('gulp-uglify/composer');
 const cssnano = require('gulp-cssnano');
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
+const uglifyjs = require('uglify-es');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const webpackStream = require('webpack-stream');
-const composer = require('gulp-uglify/composer');
-const uglifyjs = require('uglify-es');
 
 
 const minify = composer(uglifyjs, console);
 
-gulp.task('watch', function() {
+gulp.task('watch', ['browserSync'], function() {
   gulp.watch('src/jsx/*', ['build:react']);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
 });
 
 gulp.task('build', ['build:react']);
@@ -23,4 +26,12 @@ gulp.task('build:react', () => {
     .pipe(gulpIf('*.js', minify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('./src/js'));
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src',
+    },
+  });
 });
